3b4ef30dbaad17b19e64a8868c860f33b50476d4
lxx_linux <lxx02@ubuntu.(none)>
2015-09-02 01:15:49 -0700

    test1

---
+++ app/src/main/java/com/example/lxx02/myapplication/dsfduifsdi.java
@@ -0,0 +1,11 @@
+package com.example.lxx02.myapplication;
+
+/**
+ * Created by lxx02 on 9/2/15.
+ */
+public class dsfduifsdi {
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+}
