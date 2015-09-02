92f41bdf5f9b47ad220a4c8504f47e438044c707
lxx02 <853327415@qq.com>
2015-09-02 01:26:23 -0700

    test1

--- app/src/main/java/com/example/lxx02/myapplication/dsfduifsdi.java
+++ app/src/main/java/com/example/lxx02/myapplication/dsfduifsdi.java
@@ -4,11 +4,8 @@ package com.example.lxx02.myapplication;
  * Created by lxx02 on 9/2/15.
  */
 public class dsfduifsdi {
-<<<<<<< HEAD
     @Override
     public boolean equals(Object o) {
         return super.equals(o);
     }
-=======
->>>>>>> origin/master
 }
